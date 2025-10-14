6c32dc7a2f5f5780435be4a98dfd88d3028d9814
uncle12genius <omondievan461@gmail.com>
2025-10-14 13:28:22 +0300

    if else and else if

---
+++ src/elseif.java
@@ -0,0 +1,15 @@
+public class elseif {
+     public static void main (String[]args ){
+         int age = 18;
+
+         if (age <=25 ){
+             System.out.println("You not allowed to vote ");
+         }
+         else if( age <= 17) {
+             System.out.println("you are not allowed to vote take not ");
+         }
+         else {
+             System.out.println("You are allowed to vote  register for the future ");
+         }
+     }
+}
---
+++ src/ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main (String[]args){
+        int age = 18;
+         if (age <= 18){
+             System.out.println("You are not allowed to vote ");
+         }
+         else {
+             System.out.println(" you are allowed to vote ");
+         }
+    }
+}
