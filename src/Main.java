45df59fdbc9630be5b1ac5d9845e562a8371a533
uncle12genius <omondievan461@gmail.com>
2025-10-14 11:40:58 +0300

    switch statement

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main (String[] args){
+        int age = 18;
+
+        switch (age) {
+            case 1 :
+                System.out.println("You are not eliable to vote ");
+                break;
+
+            case 2 :
+                System.out.println("You now allowed to vote ");
+                break;
+
+            default:
+                System.out.println("you are not in any category ");
+        }
+    }
+
+}
