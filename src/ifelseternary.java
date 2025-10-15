99e9c5dde8a319bce12ea6c2420c8ecab62116f0
uncle12genius <omondievan461@gmail.com>
2025-10-15 08:44:04 +0300

    if else tenary

---
+++ src/ifelseternary.java
@@ -0,0 +1,8 @@
+class Ifelseternary {
+    public static void main (String[]args){
+        int Time = 20 ;
+        String Results;
+        Results = (Time < 18) ? "Good day" :"Good evening ";
+        System.out.println(Results);
+    }
+}
