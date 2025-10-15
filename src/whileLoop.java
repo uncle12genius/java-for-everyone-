5c8b5b5b1d045f1a5d64d25dbf37ae5c7584cc6a
uncle12genius <omondievan461@gmail.com>
2025-10-15 15:37:33 +0300

    while with an increment

--- src/whileLoop.java
+++ src/whileLoop.java
@@ -1,11 +1,12 @@
 class whileLoop {
     public static void main (String[]args){
-        int countdown = 4 ;
-         while (countdown > 0 ){
-             System.out.println(countdown);
-             countdown --;
-         }
-         System.out.println("Happy new year ");
-    }
+        int countdown = 5 ;
 
+        while (countdown<= 7){
+            System.out.println( "countdown :"+countdown);
+            countdown ++;
+
+        }
+        System.out.println("lights off ");
+    }
 }
