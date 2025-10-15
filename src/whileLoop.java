696e13ee4eedd1c5e790672328d9fe961b406cf7
uncle12genius <omondievan461@gmail.com>
2025-10-15 10:06:16 +0300

    while loop

---
+++ src/whileLoop.java
@@ -0,0 +1,11 @@
+class whileLoop {
+    public static void main (String[]args){
+        int countdown = 4 ;
+         while (countdown > 0 ){
+             System.out.println(countdown);
+             countdown --;
+         }
+         System.out.println("Happy new year ");
+    }
+
+}
