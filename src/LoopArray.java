11711d442734a00ce70ed71fff5097718bd17e6f
uncle12genius <omondievan461@gmail.com>
2025-10-16 10:30:36 +0300

    loop through an array

---
+++ src/LoopArray.java
@@ -0,0 +1,8 @@
+public class LoopArray {
+    public static void main (String[]args){
+        String [] cars = {"BMW ", " VOLVO","TOYOTA ", "MAZDA ", "ASTRON MARTIN ", "FERARRI "};
+        for (int i  = 0 ; i < cars.length;i++  ){
+            System.out.println(cars[i]);
+        }
+    }
+}
