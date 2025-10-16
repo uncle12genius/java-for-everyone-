3ba5dc72b48c9187e99cdf3423ee727df0a052d0
uncle12genius <omondievan461@gmail.com>
2025-10-16 10:11:16 +0300

    getting the length of an array and accessing

---
+++ src/ArrayElement.java
@@ -0,0 +1,9 @@
+public class ArrayElement {
+    public static void main (String[]args){
+        String[]cars ={"volovo", "Toyota ","Benz ", "BMW"};
+        cars[0]= "opel ";
+        System.out.println(cars[0]);
+        System.out.println(cars.length);
+
+    }
+}
