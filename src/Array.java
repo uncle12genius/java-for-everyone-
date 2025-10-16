c6e8dd8cf532824d3083930a9aa320e66836c58a
uncle12genius <omondievan461@gmail.com>
2025-10-16 09:00:24 +0300

    array

---
+++ src/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main (String[]args){
+        String[]cars = {"BMW", "Toyota ","Aston Martin ", "Mazda "};
+        System.out.println(cars[2]);
+        System.out.println(cars[3]);
+
+    }
+}
