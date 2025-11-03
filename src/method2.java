fd8c55f3f74287b423f9e9eb208ffe84ead58142
uncle12genius <omondievan461@gmail.com>
2025-11-03 22:29:04 +0300

    methods in Java

---
+++ src/method2.java
@@ -0,0 +1,11 @@
+public class method2 {
+    static void myMethod (String fname){
+        System.out.println (fname + "evan");
+    }
+    public static void main (String[]args){
+        myMethod("omondi ");
+        myMethod( "james");
+        myMethod( "jambo");
+    }
+
+}
---
+++ src/methods.java
@@ -0,0 +1,8 @@
+public class methods {
+    static void myMethod (){
+        System.out.println ("I just got executed ");
+    }
+    public static void main (String[] args){
+        myMethod();
+    }
+}
