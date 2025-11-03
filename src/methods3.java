35b0befc9265545cbd82027fc99583ba29a6729a
uncle12genius <omondievan461@gmail.com>
2025-11-03 23:09:50 +0300

    setting a method parameter with if else statement

---
+++ src/methods3.java
@@ -0,0 +1,15 @@
+public class methods3 {
+    static void checkAge (int age ){
+        if (age < 18 ){
+            System.out.println("You are allowed to vote ");
+        } else {
+            System.out.println("You are  not allowed to vote ");
+        }
+
+        }
+
+        public static void main (String[]args){
+        checkAge(20);
+        }
+    }
+
