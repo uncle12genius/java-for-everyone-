130a54dfa72357d8f65af4dfb5afb6ab3600d696
uncle12genius <omondievan461@gmail.com>
2025-11-05 18:46:44 +0300

    Class Attribute

---
+++ src/classAttribute.java
@@ -0,0 +1,8 @@
+public class classAttribute {
+    int x =60;
+    public static void main (String[] args){
+        classAttribute Attribute = new classAttribute();
+        Attribute.x=56;
+        System.out.println(Attribute.x);
+    }
+}
