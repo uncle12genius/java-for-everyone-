7d30bab30818d72ef2c7ea6f3ec2be9cefb142f8
uncle12genius <omondievan461@gmail.com>
2025-11-05 17:16:56 +0300

    class and objects in Java

---
+++ src/classJava.java
@@ -0,0 +1,13 @@
+public class classJava {
+    int x =50;
+    // class in java
+    public static void main (String[]args){
+        classJava myObj = new classJava();
+        classJava myObj2 = new classJava();
+        System.out.println(myObj.x);
+        System.out.println(myObj2.x);
+
+        // the object in java is created in we specify the class name , followed by the object name and use the key the new key word
+
+    }
+}
