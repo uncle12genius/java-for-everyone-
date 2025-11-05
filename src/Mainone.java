011dbdb35dbf19fd1857c16dbf6d097463371487
uncle12genius <omondievan461@gmail.com>
2025-11-05 18:29:59 +0300

    accessing an object from a different class

---
+++ src/Mainone.java
@@ -0,0 +1,3 @@
+public class Mainone {
+    int x = 5;
+}
---
+++ src/Second.java
@@ -0,0 +1,6 @@
+public class Second {
+    public static void main (String[]args){
+        Mainone myObj = new Mainone();
+        System.out.println(myObj.x);
+    }
+}
